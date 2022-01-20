a72819a1a01dbbfbd1e6fb15021c2ae9951bda51
rafalwrona <wronarafal@gmail.com>
2022-01-20 13:47:02 +0100

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * Złap kropkę gdy jest na  środku
+ */
+input.onButtonPressed(Button.A, function () {
+    if (sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else {
+        game.gameOver()
+    }
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 2)
+basic.forever(function () {
+    sprite.move(1)
+    sprite.ifOnEdgeBounce()
+    basic.pause(500)
+})
