b1005d705c4198d9223e85dea9965431bfc94b3b
rafalwrona <wronarafal@gmail.com>
2022-02-08 17:09:48 +0100

    levele gry

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-/**
- * Złap kropkę gdy jest na  środku
- */
 input.onButtonPressed(Button.A, function () {
     if (sprite.get(LedSpriteProperty.X) == 2) {
         game.addScore(1)
@@ -10,8 +7,20 @@ input.onButtonPressed(Button.A, function () {
 })
 let sprite: game.LedSprite = null
 sprite = game.createSprite(2, 2)
+game.setScore(0)
+/**
+ * Złap kropkę gdy jest na  środku
+ */
 basic.forever(function () {
     sprite.move(1)
     sprite.ifOnEdgeBounce()
-    basic.pause(500)
+    if (game.score() <= 5) {
+        basic.pause(500)
+    } else if (game.score() > 5 && game.score() <= 10) {
+        basic.pause(400)
+    } else if (game.score() > 10 && game.score() < 20) {
+        basic.pause(300)
+    } else {
+        basic.pause(200)
+    }
 })
